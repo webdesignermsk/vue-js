c9e7ed96baf634fc86f04314c22b8455d35c271e
Sergey Pozdniakov <webdesignermsk@mail.ru>
2018-10-05 20:41:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  REST_ENDPOINT: JSON.stringify('https://wptavern.com/wp-json/wp/v2'),
+  REST_ENDPOINT: JSON.stringify('https://bison-studio.com/bison-backend/wp-json/wp/v2/'),
   POSTS_PER_PAGE: JSON.stringify(9),
   GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-111438908-1' : 'XXX'),
   REQUEST_CACHE_MAX: JSON.stringify(150)
